6260af5a92931d48cb434e2220d1c60029985c8b
Vladimir Hristov <31288155+vladichhh@users.noreply.github.com>
2018-03-28 00:59:40 +0300

    Update Ownable.sol

--- project/marketplace/contracts/Ownable.sol
+++ project/marketplace/contracts/Ownable.sol
@@ -1,1 +1,24 @@
+pragma solidity 0.4.19;
 
+contract Ownable {
+    
+    event OwnershipTransferred(address previousOwner, address newOwner);
+    
+    address public owner;
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function Ownable() public {
+        owner = msg.sender;
+    }
+    
+    function transferOwnership(address newOwner) public {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+    
+}
